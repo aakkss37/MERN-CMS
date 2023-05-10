c9047582a508016ca377e09c27a52941b5820f09
amar kumar <amarkumar.sharma.124@gmail.com>
2023-05-10 19:29:11 +0530

    route set

--- server/controller/homePageController/bannerController.js
+++ server/controller/homePageController/bannerController.js
@@ -1,7 +1,44 @@
 import BannerData from "../../model/HomePage/bannerSchema.js"
 
 
-export const updateTitle = async (request, response) => {
-	console.log("request recieved")
-	response.status(200).json({ msg: "Title updated sucessfully."});
+export const updateBannerTitle = async (request, response) => {
+	try {
+		const upateTitle = await BannerData.findByIdAndUpdate(request.body.id, 
+			{
+				title: request.body.title
+			}
+		)
+
+		response.status(200).json({ msg: "Banner Title Updated sucessfully.", data: upateTitle });
+	} catch (error) {
+		response.status(500).json({ msg: "Error while Updating Title." });
+	}
+}
+
+export const updateBannerText = async (request, response) => {
+	try {
+		const upateTitle = await BannerData.findByIdAndUpdate(request.body.id, 
+			{
+				title: request.body.title
+			}
+		)
+
+		response.status(200).json({ msg: "Banner Title Updated sucessfully.", data: upateTitle });
+	} catch (error) {
+		response.status(500).json({ msg: "Error while Updating Title." });
+	}
+}
+
+export const updateBannerImage = async (request, response) => {
+	try {
+		const upateTitle = await BannerData.findByIdAndUpdate(request.body.id, 
+			{
+				title: request.body.title
+			}
+		)
+
+		response.status(200).json({ msg: "Banner Title Updated sucessfully.", data: upateTitle });
+	} catch (error) {
+		response.status(500).json({ msg: "Error while Updating Title." });
+	}
 }
--- server/router/router.js
+++ server/router/router.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { updateTitle } from "../controller/homePageController/bannerController.js";
+import { updateBannerImage, updateBannerText, updateBannerTitle, } from "../controller/homePageController/bannerController.js";
 const router = express.Router();
 
 
-router.post('/home-page/banner/update/title', updateTitle)
+
+// BANNER 
+router.post('/home-page/banner/update/title', updateBannerTitle)
+router.post('/home-page/banner/update/text', updateBannerText)
+router.post('/home-page/banner/update/image', updateBannerImage)
 
 
 export default router
