be6093dfe3994869f13404a51cf2992d06b21541
amar kumar <amarkumar.sharma.124@gmail.com>
2023-05-17 14:56:00 +0530

    modification in partners page size

--- Agile-Global-source-code/src/components/pages/Partner.jsx
+++ Agile-Global-source-code/src/components/pages/Partner.jsx
@@ -24,26 +24,26 @@ const Partner = () => {
   return (
     <>
       <div data-aos="fade-up" className='px-3 flex flex-col items-center justify-center gap-2 sm:px-5 md:px-10 lg:px-20 xl:px-44 mt-10'>
-        <p className='text-[32px] font-bold sm:text-[40px] md:text-[48px] text-center leading-[58px]  '>OUR PARTNER  MODEL</p>
-        <p className='text-[20px] sm:text-[24px] leading-[30px] text-center'>Our clients range from large Fortune 1000 firms to small businesses as well as several State, County and Local government agencies across the United States. Many of our customers are referrals from our existing clientele. Agile Global has a client repeat engagement rate of 98% and D&B Customer approval rating of 94%. </p>
+        <p className='text-[32px] font-bold sm:text-[40px] md:text-[40px] text-center leading-[58px]  '>OUR PARTNER  MODEL</p>
+        <p className='text-[20px] sm:text-[20px] leading-[30px] text-center'>Our clients range from large Fortune 1000 firms to small businesses as well as several State, County and Local government agencies across the United States. Many of our customers are referrals from our existing clientele. Agile Global has a client repeat engagement rate of 98% and D&B Customer approval rating of 94%. </p>
       </div>
 
 
       {/* clients logos  */}
       <div data-aos="fade-up" className='flex flex-col sm:flex-row flex-wrap items-center justify-center gap-3 mt-10 sm:mt-0'>
-        <img src={homeClient1} alt="" />
-        <img src={homeClient2} alt="" />
+        <img src={homeClient1} alt="" className='w-[240px] h-[190px]'/>
+        <img src={homeClient2} alt="" className='w-[240px] h-[190px]'/>
 
-        <img src={homeClient4} alt="" />
-        <img src={homeClient3} alt="" />
+        <img src={homeClient4} alt="" className='w-[240px] h-[190px]'/>
+        <img src={homeClient3} alt="" className='w-[240px] h-[190px]'/>
       </div>
       {/* clients logo 2nd div */}
       <div data-aos="fade-up" className='hidden lg:flex flex-col sm:flex-row flex-wrap items-center justify-center gap-3 mt-10 sm:mt-[-100px]'>
-        <img src={homeClient4} alt="" />
-        <img src={homeClient3} alt="" />
+        <img src={homeClient4} alt="" className='w-[240px] h-[190px]'/>
+        <img src={homeClient3} alt="" className='w-[240px] h-[190px]'/>
 
-        <img src={homeClient1} alt="" />
-        <img src={homeClient2} alt="" />
+        <img src={homeClient1} alt="" className='w-[240px] h-[190px]'/>
+        <img src={homeClient2} alt="" className='w-[240px] h-[190px]'/>
       </div>
 
 
@@ -51,18 +51,18 @@ const Partner = () => {
         ShowMoreClientLogo &&
         // {/* clients logo 2nd div */}
         <div data-aos="fade-up" className=' flex flex-col sm:flex-row flex-wrap items-center justify-center gap-3 mt-10 sm:mt-[-100px]'>
-          <img src={homeClient3} alt="" />
-          <img src={homeClient1} alt="" />
+          <img src={homeClient3} alt="" className='w-[240px] h-[190px]'/>
+          <img src={homeClient1} alt="" className='w-[240px] h-[190px]'/>
 
-          <img src={homeClient2} alt="" />
-          <img src={homeClient4} alt="" />
+          <img src={homeClient2} alt="" className='w-[240px] h-[190px]'/>
+          <img src={homeClient4} alt="" className='w-[240px] h-[190px]'/>
         </div>
       }
 
       {/* Swipe down to see more companies */}
       <div data-aos="fade-up" onClick={() => setShowMoreClientLogo(prev => !prev)} className='cursor-pointer flex items-center justify-center flex-col gap-0 '>
-        {ShowMoreClientLogo ? <IoIosArrowUp className='font-bold text-[48px]'></IoIosArrowUp> : null}
-        <p className='text-[20px] text-center leading-[30px]'>Click to see {ShowMoreClientLogo ? "less" : "more"} companies</p>
+        {ShowMoreClientLogo ? <IoIosArrowUp className='font-bold text-[40px]'></IoIosArrowUp> : null}
+        <p className='text-[16px] text-center leading-[30px]'>Click to see {ShowMoreClientLogo ? "less" : "more"} companies</p>
         {ShowMoreClientLogo ? null : <img src={upArrow} alt="" />}
       </div>
 
@@ -70,14 +70,14 @@ const Partner = () => {
 
       {/* text p */}
       <div data-aos="fade-up" className='flex items-center justify-center px-3 sm:px-5 md:px-10 lg:px-20 xl:px-44 mt-10 mb-24'>
-        <p className='text-[20px] sm:text-[24px] leading-[30px] text-center   '>AGILE GLOBAL has assisted numerous firms in their application development, integration, conversion, consolidation and support efforts. We are also dedicated to partnering with early stage and emerging growth enterprise software companies, working with them to bring to market the best technology solutions possible.</p>
+        <p className='text-[18px] sm:text-[18px] leading-[30px] text-center   '>AGILE GLOBAL has assisted numerous firms in their application development, integration, conversion, consolidation and support efforts. We are also dedicated to partnering with early stage and emerging growth enterprise software companies, working with them to bring to market the best technology solutions possible.</p>
       </div>
 
 
 
       {/* Our Recognitions & Awards */}
       <div data-aos="fade-up" className=' flex flex-col sm:flex-row flex-wrap items-center sm:items-start justify-center gap-9 mt-16'>
-        <p className='text-[30px] sm:text-[40px] md:text-[48px] leading-[58px] font-semibold text-center sm:text-start sm:w-[300px]'>Our
+        <p className='text-[30px] sm:text-[40px] md:text-[40px] leading-[58px] font-semibold text-center sm:text-start sm:w-[300px]'>Our
           <span className='text-[#17519B] '> Recognitions</span> & Awards</p>
 
         {/* card 1 */}
@@ -85,7 +85,7 @@ const Partner = () => {
           {/* <img src={RecogImg1} alt="" /> */}
           {/* black bg div */}
           <div className='bg-[#0000007a] transition-all-2s partner-recog-card-inner rounded-b-[20px]  px-5 py-5 absolute bottom-0'>
-            <p className='text-[20px] text-center leading-[24px] text-white'>Ranked #3 Top IT Consulting Firm</p>
+            <p className='text-[18px] text-center leading-[24px] text-white'>Ranked #3 Top IT Consulting Firm</p>
           </div>
         </div>
 
@@ -94,7 +94,7 @@ const Partner = () => {
           {/* <img src={RecogImg1} alt="" /> */}
           {/* black bg div */}
           <div className='bg-[#0000007a] transition-all-2s partner-recog-card-inner rounded-b-[20px]  px-5 py-5 absolute bottom-0'>
-            <p className='text-[20px] text-center leading-[24px] text-white'>Ranked #3 Top IT Consulting Firm</p>
+            <p className='text-[18px] text-center leading-[24px] text-white'>Ranked #3 Top IT Consulting Firm</p>
           </div>
         </div>
 
@@ -104,7 +104,7 @@ const Partner = () => {
           {/* <img src={RecogImg1} alt="" /> */}
           {/* black bg div */}
           <div className='bg-[#0000007a] transition-all-2s partner-recog-card-inner rounded-b-[20px]  px-5 py-5 absolute bottom-0'>
-            <p className='text-[20px] text-center leading-[24px] text-white'>Ranked #3 Top IT Consulting Firm</p>
+            <p className='text-[18px] text-center leading-[24px] text-white'>Ranked #3 Top IT Consulting Firm</p>
           </div>
         </div>
 
