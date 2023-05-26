3da8ebc606eb2c8c5d9a71378a22664d21ab169b
amar kumar <amarkumar.sharma.124@gmail.com>
2023-05-26 19:23:37 +0530

    services page compelte

--- cms-admin-dashboard/src/routes.js
+++ cms-admin-dashboard/src/routes.js
@@ -57,6 +57,7 @@ const Page404 = React.lazy(() => import('./pages/page404/Page404'))
 // CMS Items
 const HomePageCMS = React.lazy(()=> import('./pages/CMS/Home/Home'))
 const HomeDriverPageCMS = React.lazy(()=> import('./pages/CMS/Home/Drivers/Drivers'))
+const HomeServicesPageCMS = React.lazy(()=> import('./pages/CMS/Home/Services/Services'))
 const PartnerModelCMS = React.lazy(()=> import('./pages/CMS/PartnerModel/PartnerModel'))
 const ContactUsPageCMS = React.lazy(()=> import('./pages/CMS/ContactUs/ContactUs'))
 const CareerPageCMS = React.lazy(()=> import('./pages/CMS/Career/Career'))
@@ -115,6 +116,7 @@ const routes = [
 	/********* HOME *********/
 	{ path: '/cms/home', name: 'Home', element: HomePageCMS },
 	{ path: '/cms/home/drivers', name: 'Driver', element: HomeDriverPageCMS },
+	{ path: '/cms/home/services', name: 'Services', element: HomeServicesPageCMS },
 
 	/********* SOLUTION *********/
 
