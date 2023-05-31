04929b81cb68459d331b07559d29af256b9b2c63
amal_436 <amalshakya436@gmail.com>
2023-05-31 19:25:57 +0530

    recognition and awards apis change

--- server/index.js
+++ server/index.js
@@ -4,8 +4,11 @@ import cors from 'cors';
 import dotenv from 'dotenv';
 import ErrorHandler from './middleware/error.js';
 import cookieParser from 'cookie-parser';
-import swaggerJsdoc from 'swagger-jsdoc';
 import swaggerUi from 'swagger-ui-express'
+import YAML from 'yamljs';
+// import swaggerJsdoc from 'swagger-jsdoc';
+
+const swaggerJsdoc = YAML.load('./api.yaml');
 
 dotenv.config();
 
@@ -13,26 +16,26 @@ dotenv.config();
 const app = express()
 
 // Swagger configuration
-const swaggerOptions = {
-	swaggerDefinition: {
-		openapi: '3.0.0', // Update to OpenAPI 3.0.0 version
-		info: {
-			title: 'Swagger Demo API',
-			description: 'A sample API for demonstrating Swagger',
-			version: '1.0.0',
-		},
-		servers: [
-			{
-				url: 'http://localhost:8000', // Update the server URL
-				description: 'Local server',
-			},
-		],
-	},
-	apis: ['./router/router.js'], // Specify the file that contains your API routes
-};
+// const swaggerOptions = {
+// 	swaggerDefinition: {
+// 		openapi: '3.0.0', // Update to OpenAPI 3.0.0 version
+// 		info: {
+// 			title: 'Swagger Demo API',
+// 			description: 'A sample API for demonstrating Swagger',
+// 			version: '1.0.0',
+// 		},
+// 		servers: [
+// 			{
+// 				url: 'http://localhost:8000', // Update the server URL
+// 				description: 'Local server',
+// 			},
+// 		],
+// 	},
+// 	apis: ['./router/router.js'], // Specify the file that contains your API routes
+// };
 
-const swaggerSpec = swaggerJsdoc(swaggerOptions);
-app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+// const swaggerSpec = swaggerJsdoc(swaggerOptions);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerJsdoc));
 
 
 // APP CONFIGURATION
--- server/middleware/auth.js
+++ server/middleware/auth.js
@@ -5,13 +5,14 @@ import jwt from "jsonwebtoken";
 
 
 export const isAuthenticatedUser = catchAsyncError(async (req, res, next) => {
-    const token  = req.headers['authorization'];
-    console.log(token);
+    const token = req.headers['authorization'];
     if (!token) {
         return next(new ErrorHandler("Please login for access these resources", 401));
     }
 
     const decodedData = jwt.verify(token, process.env.JWT_SECRET_KEY);
+
+    console.log(decodedData);
 
     req.user = await userSchema.findById(decodedData.id);
     next();
